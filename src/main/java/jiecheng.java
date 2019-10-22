7f80679dae623cc14860c33d6391ad0cb21914e0
wumutong <wumutong121@163.com>
2019-10-22 10:55:52 +0800

    首项目

---
+++ src/main/java/jiecheng.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class jiecheng {
+    public static BigDecimal factorial(BigDecimal n){
+        BigDecimal bd1 = new BigDecimal(1);//BigDecimal类型的1
+        BigDecimal bd2 = new BigDecimal(2);//BigDecimal类型的2
+        BigDecimal result = bd1;//结果集，初值取1
+        while(n.compareTo(bd1) > 0){//参数大于1，进入循环
+            result = result.multiply(n.multiply(n.subtract(bd1)));//实现result*（n*（n-1））
+            n = n.subtract(bd2);//n-2后继续
+        }
+        return result;
+    }
+    public static void main(String[] arguments){
+      /*  Scanner sc = new Scanner(System.in);
+        BigDecimal n = sc.nextBigDecimal();
+        System.out.print(n + "!=" + factorial(n));*/
+
+        BigDecimal a = new BigDecimal(2);
+        BigDecimal b = new BigDecimal(3);
+        BigDecimal c = new BigDecimal(4);
+
+        c=a.add(b);
+        System.out.println(c);
+
+    }
+
+}
