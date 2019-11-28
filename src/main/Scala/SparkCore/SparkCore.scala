e7e9db4a7a08da557a9ec78e6943e6185dffcc13
wumutong <wumutong121@163.com>
2019-11-28 23:05:34 +0800

    使用Scala中的SparkCore实验WordCount

--- src/main/Scala/SparkCore/SparkCore.scala
+++ src/main/Scala/SparkCore/SparkCore.scala
@@ -10,16 +10,12 @@ object SparkCore {
     //加载文件中的单词
     val spark = new SparkContext(new SparkConf()
       .setMaster("local[*]").setAppName("wordCount"))
-      .textFile("src\\main\\File\\Word.txt")
+      .textFile("src\\resources\\Word.txt")
+
 
     val tuples: Array[(String, Int)] = spark.flatMap(_.split(" ")).map((_, 1))
       .reduceByKey(_ + _).sortBy(_._2, false).collect()
 
 
-
-
-
   }
-
-
 }
